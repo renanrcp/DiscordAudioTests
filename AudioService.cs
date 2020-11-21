4ce24c651f7827b46983b633fcd4681f658fba75
Renan Pinheiro <renancamerapinheiro@gmail.com>
2020-11-21 17:11:01 -0300

    stablishing voice gateway websocket connection (very weird code)

--- AudioService.cs
+++ AudioService.cs
@@ -8,11 +8,86 @@ using Discord;
 using Discord.Audio;
 using Discord.WebSocket;
 using Microsoft.Extensions.Hosting;
+using System.Text.Json;
+using System.Text;
 
 namespace DiscordAudioTests
 {
     public class AudioService
     {
+        public class Payload
+        {
+            public Payload()
+            {
+            }
+
+            public Payload(int op)
+            {
+                this.op = op;
+            }
+
+            public int op { get; set; }
+        }
+
+        public class Payload<T> : Payload
+        {
+            public Payload()
+            {
+            }
+
+            public Payload(int op, T d) : base(op)
+            {
+                this.d = d;
+            }
+
+            public T d { get; set; }
+        }
+
+        public class VoiceIdentifyPayload
+        {
+            public VoiceIdentifyPayload()
+            {
+            }
+
+            public VoiceIdentifyPayload(ulong server_id, ulong user_id, string session_id, string token)
+            {
+                this.server_id = server_id;
+                this.user_id = user_id;
+                this.session_id = session_id;
+                this.token = token;
+            }
+
+            public ulong server_id { get; set; }
+            public ulong user_id { get; set; }
+            public string session_id { get; set; }
+            public string token { get; set; }
+        };
+
+        public class VoiceReadyPayload
+        {
+            public int ssrc { get; set; }
+
+            public string ip { get; set; }
+
+            public int port { get; set; }
+
+            public string[] modes { get; set; }
+        }
+
+        public class HelloPayload
+        {
+            public HelloPayload()
+            {
+            }
+
+            public HelloPayload(float heartbeat_interval)
+            {
+                this.heartbeat_interval = heartbeat_interval;
+            }
+
+            public float heartbeat_interval { get; set; }
+        }
+
         private const string AudioFilePath = "Marshmello & Anne-Marie - FRIENDS (Lyric Video) _OFFICIAL FRIENDZONE ANTHEM.mp3";
 
         private readonly DiscordShardedClient _client;
@@ -60,6 +135,39 @@ namespace DiscordAudioTests
             var sessionId = await sessionIdTsc.Task;
             var voiceServer = await socketVoiceServerTsc.Task;
 
+            var webSocket = new ClientWebSocket();
+
+            var connectionUri = new Uri($"wss://{voiceServer.Endpoint}?v=4");
+
+            await webSocket.ConnectAsync(connectionUri, default);
+
+            var helloPayloadBytes = new byte[1024].AsMemory();
+
+            var helloResult = await webSocket.ReceiveAsync(helloPayloadBytes, default);
+
+            helloPayloadBytes = helloPayloadBytes.Slice(0, helloResult.Count);
+
+            var helloPayload = JsonSerializer.Deserialize<Payload<HelloPayload>>(helloPayloadBytes.Span);
+
+            var voiceIdentifyPayload = new Payload<VoiceIdentifyPayload>(0, new VoiceIdentifyPayload(guildId, _client.CurrentUser.Id, sessionId, voiceServer.Token));
+
+            var voiceIdentifyPayloadJson = JsonSerializer.SerializeToUtf8Bytes(voiceIdentifyPayload);
+
+            await webSocket.SendAsync(voiceIdentifyPayloadJson, WebSocketMessageType.Text, true, default);
+
+            var voiceReadyPayloadBytes = new byte[1024].AsMemory();
+
+            var voiceIdentityResult = await webSocket.ReceiveAsync(voiceReadyPayloadBytes, default);
+
+            voiceReadyPayloadBytes = voiceReadyPayloadBytes.Slice(0, voiceIdentityResult.Count);
+
+            var voiceReadyPayload = JsonSerializer.Deserialize<Payload<VoiceReadyPayload>>(voiceReadyPayloadBytes.Span);
+
+            var heartBeatPayload = new Payload<float>(3, helloPayload.d.heartbeat_interval);
+
+            var heartBeatPayloadJson = JsonSerializer.SerializeToUtf8Bytes(heartBeatPayload);
+
+            await webSocket.SendAsync(heartBeatPayloadJson, WebSocketMessageType.Text, true, default);
 
             await voiceChannel.DisconnectAsync();
 
@@ -67,7 +175,7 @@ namespace DiscordAudioTests
             _client.VoiceServerUpdated -= VoiceServerUpdatedAsync;
         }
 
-        private Process CreateStream(string path)
+        private Process CreateFFmpegStream(string path)
         {
             return Process.Start(new ProcessStartInfo
             {
