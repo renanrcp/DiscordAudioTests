4c15416d1436fd90bae73a38ae1fc7ef1a0fb6cd
Renan Pinheiro <renancamerapinheiro@gmail.com>
2020-11-22 02:15:01 -0300

    multiple jsons reading from buffer

--- Websockets/VoiceGatewayClient.Read.cs
+++ Websockets/VoiceGatewayClient.Read.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Buffers;
+using System.Collections.Generic;
 using System.IO.Pipelines;
 using System.Net.WebSockets;
 using System.Text;
@@ -44,10 +45,13 @@ namespace DiscordAudioTests.Websockets
 
                     var buffer = readResult.Buffer;
 
-                    ReceiverReader.AdvanceTo(buffer.End);
+                    if (TryParseJsons(buffer, out var jsonPayloads))
+                    {
+                        foreach (var jsonPayload in jsonPayloads)
+                            await ProcessPayloadAsync(jsonPayload);
+                    }
 
-                    if (TryParseJson(buffer, out var jsonPayload))
-                        _ = Task.Run(() => ProcessPayloadAsync(jsonPayload));
+                    ReceiverReader.AdvanceTo(buffer.Start, buffer.End);
                 }
             }
             finally
@@ -70,6 +74,70 @@ namespace DiscordAudioTests.Websockets
             }
 
             return await ReceiverWriter.FlushAsync(cancellationToken);
+        }
+
+        private bool TryParseJsons(ReadOnlySequence<byte> buffer, out IEnumerable<JsonDocument> jsonDocuments)
+        {
+            var jsons = new List<JsonDocument>();
+            SequencePosition? firstPosition = null;
+            SequencePosition? lastPosition = null;
+
+            do
+            {
+                if (buffer.Length <= 0)
+                    break;
+
+                firstPosition = GetJsonStartPosition(buffer);
+                lastPosition = GetJsonEndPosition(buffer);
+
+                if (firstPosition != null && lastPosition != null)
+                {
+                    var jsonBuffer = buffer.Slice(firstPosition.Value, lastPosition.Value);
+
+                    if (!TryParseJson(jsonBuffer, out var jsonDocument))
+                    {
+                        jsonDocuments = jsons;
+                        return false;
+                    }
+
+                    jsons.Add(jsonDocument);
+
+                    buffer = buffer.Slice(lastPosition.Value);
+                }
+            }
+            while (lastPosition != null);
+
+            jsonDocuments = jsons;
+            return jsons.Count > 0;
+        }
+
+        private SequencePosition? GetJsonStartPosition(ReadOnlySequence<byte> buffer)
+            => buffer.PositionOf((byte)'{');
+
+        private SequencePosition? GetJsonEndPosition(ReadOnlySequence<byte> buffer)
+        {
+            var sum = 0;
+            var startPosition = buffer.Start;
+
+            if (!buffer.TryGet(ref startPosition, out var memoryBuffer))
+                return null;
+
+            while (true)
+            {
+                var jsonFinalPosition = memoryBuffer.Span.IndexOf((byte)'}') + 1;
+
+                if (memoryBuffer.Span.Length <= jsonFinalPosition)
+                    return buffer.GetPosition(jsonFinalPosition + sum);
+
+                var nextValue = memoryBuffer.Span[jsonFinalPosition];
+
+                if (nextValue == (byte)'{')
+                    return buffer.GetPosition(jsonFinalPosition + sum);
+
+                memoryBuffer = memoryBuffer.Slice(jsonFinalPosition);
+
+                sum += jsonFinalPosition;
+            }
         }
 
         private bool TryParseJson(ReadOnlySequence<byte> buffer, out JsonDocument jsonDocument)
